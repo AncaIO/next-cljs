80b40a19541a08d0e26c790f5899c77cdf2bd124
Anca Emcken <1617798+ancaemcken@users.noreply.github.com>
2022-05-14 15:25:36 +0200

    Rename layout function, add example to another page

--- src/main/pages/website_about.cljs
+++ src/main/pages/website_about.cljs
@@ -1,6 +1,7 @@
 (ns pages.website-about
   (:require
    [reagent.core :as r]
+   [shadow.next-js :as sn]
    ["next/link" :default Link]))
 
 (defn page-about
@@ -10,5 +11,10 @@
   (r/as-element
    [:div
     [:h1 "About us"]
-
     [:> Link {:href "/" :passhref true} [:a "back to index"]]]))
+
+(defn define-page-layout
+  [page]
+  (r/as-element [:div {:style {:background-color "cornflowerblue"}} page])) ;;???
+
+(sn/page-get-layout page-about define-page-layout)
--- src/main/pages/website_index.cljs
+++ src/main/pages/website_index.cljs
@@ -2,7 +2,6 @@
   (:require
    [reagent.core :as r]
    [shadow.next-js :as sn]
-
    ["next/link" :default Link]))
 
 (defn page-index
@@ -11,16 +10,17 @@
   [props]
   (r/as-element
    [:div
-    [:h1 "Hi people!"]
+    [:h1 "Hi people!!!"]
 
     [:p "Welcome to your new next.js site."]
     [:p "Now go build something great with ClojureScript."]
 
-    [:> Link {:href "/page-2"} [:a "goto page-2"]]]))
+    [:> Link {:href "/about"} [:a "goto page-2"]]
+    [:> Link {:href "/docs/321" :passhref true} [:a "Docs 321"]]]))
 
 
-(defn set-page-layout
+(defn define-page-layout
   [page]
   (r/as-element [:div {:style {:background-color "hotpink"}} page])) ;;???
 
-(sn/page-get-layout page-index set-page-layout)
+(sn/page-get-layout page-index define-page-layout)
