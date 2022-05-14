27b61ebf0ab34c41b74836b057c47e4d2f91a836
Anca Emcken <1617798+ancaemcken@users.noreply.github.com>
2022-05-14 15:26:39 +0200

    Clean up

--- src/main/pages/website_error_404.cljs
+++ src/main/pages/website_error_404.cljs
@@ -1,11 +1,10 @@
 (ns pages.website-error-404
   (:require
-   [reagent.core :as r]
-   ["next/link" :default Link]))
+   [reagent.core :as r]))
 
 (defn page-404
   {:export true
    :next/page "404"}
-  [props]
+  []
   (r/as-element
    [:h1 "404!"]))
